12223d47bd9361b21a0d50d6a6d604a01827d077
Paulo Xavier <paulo.ae18@gmail.com>
2026-09-24 23:40:53 -0400

    Update CACHE_NAME to version 13

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // Aumente o número a cada nova publicação: é a mudança neste arquivo que faz
 // os aparelhos detectarem e aplicarem a atualização.
-const CACHE_NAME = 'bloco-notas-shell-v12';
+const CACHE_NAME = 'bloco-notas-shell-v13';
 
 const SHELL_FILES = [
   './',
